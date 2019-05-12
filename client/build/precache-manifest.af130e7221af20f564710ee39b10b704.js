550e9c9aab0f40a2a4362b61b83df1e205cbb3d7
7seven7lst <shengtaoli0110@gmail.com>
2019-05-11 19:27:50 -0700

    [update] update

---
+++ client/build/precache-manifest.af130e7221af20f564710ee39b10b704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d7296a37a4b5c20e611",
+    "url": "/static/js/main.5d7296a3.chunk.js"
+  },
+  {
+    "revision": "d9b55224798f1b542715",
+    "url": "/static/js/1.d9b55224.chunk.js"
+  },
+  {
+    "revision": "5d7296a37a4b5c20e611",
+    "url": "/static/css/main.9af826c6.chunk.css"
+  },
+  {
+    "revision": "d9b55224798f1b542715",
+    "url": "/static/css/1.dd099dcc.chunk.css"
+  },
+  {
+    "revision": "ad02062c13b1c5c9a4dd2717803faffc",
+    "url": "/index.html"
+  }
+];
