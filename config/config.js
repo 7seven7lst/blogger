0936043ef06ed51f0c255af042f65294c7a6f498
7seven7lst <shengtaoli0110@gmail.com>
2019-05-11 19:14:21 -0700

    [update] update

--- config/config.js
+++ config/config.js
@@ -1,24 +1,26 @@
-{
-  "development": {
-    "username": "root",
-    "password": "password",
-    "database": "blogger",
-    "host": "localhost",
-    "dialect": "mysql",
-    "port": 3306
+const config = {
+  development: {
+    username: "root",
+    password: "password",
+    database: "blogger",
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
-  "production": {
-    "username": process.env.JAWSDB_USERNAME,
-    "password": process.env.JAWSDB_PASSWORD,
-    "database": process.env.JAWSDB_DB,
-    "host": process.env.JAWSDB_HOST,
-    "dialect": "mysql"
+  production: {
+    username: process.env.JAWSDB_USERNAME,
+    password: process.env.JAWSDB_PASSWORD,
+    database: process.env.JAWSDB_DB,
+    host: process.env.JAWSDB_HOST,
+    dialect: "mysql"
   }
 }
+
+module.exports = config;
